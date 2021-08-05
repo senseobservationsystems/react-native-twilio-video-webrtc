120764931badff3978b8df3a53e0b23cff6d3672
James Grant <jamesogrant@gmail.com>
2021-08-05 15:50:43 +0100

    Update TwilioVideoLocalView.android.js

--- src/TwilioVideoLocalView.android.js
+++ src/TwilioVideoLocalView.android.js
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types'
 
 const propTypes = {
   ...View.propTypes,
+  applyZOrder: PropTypes.bool,
   /**
    * How the video stream should be scaled to fit its
    * container.
