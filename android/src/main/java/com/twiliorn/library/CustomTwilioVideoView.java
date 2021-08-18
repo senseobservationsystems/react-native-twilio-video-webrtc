932c5bcb0b46122d82d255d56389500f8c2541a3
James Grant <jamesogrant@gmail.com>
2021-08-18 15:45:12 +0100

    Restored maxCaptureDimensions & maxCaptureFPS

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -309,7 +309,7 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
     // ===== SETUP =================================================================================
 
     private VideoFormat buildVideoFormat() {
-        return new VideoFormat(VideoDimensions.CIF_VIDEO_DIMENSIONS, 15);
+        return new VideoFormat(this.maxCaptureDimensions, this.maxCaptureFPS);
     }
 
     private CameraCapturer createCameraCaputer(Context context, String cameraId) {
