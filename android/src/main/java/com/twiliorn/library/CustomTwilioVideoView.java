26ff5afd7eccb5c05e3a5c28065d1c212c24d72c
James Grant <jamesogrant@gmail.com>
2023-02-24 11:27:37 +0000

    Revert changes

--- android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
+++ android/src/main/java/com/twiliorn/library/CustomTwilioVideoView.java
@@ -75,7 +75,7 @@ import com.twilio.video.VideoDimensions;
 import com.twilio.video.VideoFormat;
 import com.twilio.video.VideoCodec;
 
-import org.webrtc.voiceengine.WebRtcAudioManager;
+import tvi.webrtc.voiceengine.WebRtcAudioManager;
 
 import tvi.webrtc.Camera1Enumerator;
 import tvi.webrtc.HardwareVideoEncoderFactory;
@@ -755,8 +755,10 @@ public class CustomTwilioVideoView extends View implements LifecycleEventListene
         AudioManager audioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
         if (enabled) {
             audioManager.startBluetoothSco();
+            audioManager.setSpeakerphoneOn(false);
         } else {
             audioManager.stopBluetoothSco();
+            audioManager.setSpeakerphoneOn(true);
         }
     }
 
