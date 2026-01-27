cacb55e8818c3a4b06e84d511f1eb090a614c905
Your Name <you@example.com>
2026-01-27 16:34:49 +0700

    Fix React 19 compatibility: Remove BOOL pointer types in connect method
    - Changed BOOL * parameters to BOOL in RCTTWVideoModule.connect method
    - Fixes type conversion error: 'Error while converting JavaScript argument to Objective C type BOOL'
    - Parameters fixed: enableAudio, enableVideo, enableNetworkQualityReporting, dominantSpeakerEnabled
    - Compatible with React Native 0.81+ and React 19+

--- ios/RCTTWVideoModule.m
+++ ios/RCTTWVideoModule.m
@@ -525,7 +525,7 @@ RCT_EXPORT_METHOD(getStats) {
     }];
 }
 
-RCT_EXPORT_METHOD(connect:(NSString *)accessToken roomName:(NSString *)roomName enableAudio:(BOOL *)enableAudio enableVideo:(BOOL *)enableVideo encodingParameters:(NSDictionary *)encodingParameters enableNetworkQualityReporting:(BOOL *)enableNetworkQualityReporting dominantSpeakerEnabled:(BOOL *)dominantSpeakerEnabled cameraType:(NSString *)cameraType bandwidthProfileOptions:(NSDictionary *)bandwidthProfileOptions) {
+RCT_EXPORT_METHOD(connect:(NSString *)accessToken roomName:(NSString *)roomName enableAudio:(BOOL)enableAudio enableVideo:(BOOL)enableVideo encodingParameters:(NSDictionary *)encodingParameters enableNetworkQualityReporting:(BOOL)enableNetworkQualityReporting dominantSpeakerEnabled:(BOOL)dominantSpeakerEnabled cameraType:(NSString *)cameraType bandwidthProfileOptions:(NSDictionary *)bandwidthProfileOptions) {
   TVIVideoBandwidthProfileOptions* videoBandwidthProfile = [self prepareBandwidthProfile:bandwidthProfileOptions];
 
   [self _setLocalVideoEnabled:enableVideo cameraType:cameraType];
